4f048a59a6cec189dca02b672ef5e6e101b946a6
Gustavo <gustavo.matamoros@compsesa.com.ec>
2023-03-07 10:31:42 -0500

    Video-03-Create-ReactApp

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Casitas Felices</a>
+      </header>
+      <main>Lista de productos</main>
+    </div>
+  );
+}
+
+export default App;
