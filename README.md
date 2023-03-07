ad9f528781dc184a14b0191d226ee6cb329874ac
Gustavo <gustavo.matamoros@compsesa.com.ec>
2023-03-07 14:48:02 -0500

    Cambios generales

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 
 1. Introduccion
 2. Instalacion de herramientas
+3.
+4.
+5. Lista de productos
+   1. Crear un arreglo de productos
+   2. Agregar imagenes de los productos
+6. Agregar Routing
+   1. npm i react-router-dom
+   2. Crear ruta para la pagina home
+   3. Crear ruta para la pagina de un producto
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,28 @@
+import data from './data';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <a href="/">Casitas Felices</a>
       </header>
-      <main>Lista de productos</main>
+      <main>
+        <h1> Productos en venta</h1>
+        <div className="products">
+          {data.products.map((products) => (
+            <div className="product" key={products.slug}>
+              <a href={`/product/${products.slug}`}>
+                <img src={products.image} alt={products.name} />
+              </a>
+              <div className="product-info"></div>
+              <p>{products.name}</p>
+              <p>
+                <strong>${products.price}</strong>
+              </p>
+              <button>Agregar a carrito</button>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
